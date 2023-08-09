dd647d419450f8339d551aca12b843a683ab5262
Zilvinas <zilvinas.pusnys@gmail.com>
2023-08-09 19:49:01 +0300

    Added Game class

---
+++ src/main/java/Main/Game.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class Game {
+
+    private GamePanel gamePanel;
+    private GameWindow gameWindow;
+    public Game() {
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+    }
+}
